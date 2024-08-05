a0356c506fdbb2a1d415adf72ccb1965ce9d878c
Andriy Mahinich <andriy.m@smart-gravity.com>
2024-08-05 22:15:27 +0300

    point 128/finished

--- employees-template/src/components/employees-list/employees-list.js
+++ employees-template/src/components/employees-list/employees-list.js
@@ -1,12 +1,12 @@
 import './employees-list.css';
 
 import EmployeesListItem from '../employees-list-item/employees-list-item';
+let keyCount = 0;
 
 function EmpolyeesList(props) {
-
   let employees = props.emp_list.map(item => {
     return (
-      <EmployeesListItem {...item}/>
+      <EmployeesListItem key = {keyCount} {...item}/>
     )
   })
 
